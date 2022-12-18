b10c7dc69aa79e03da930e159d340c8df78fe947
Nikolai <nik.garkusha.91@mail.ru>
2022-12-18 19:04:34 +0300

    module7_homework
    module7_homework

---
+++ TASK 1.js
@@ -0,0 +1,7 @@
+const obj = {
+  subject: "book", 
+  name: "The_Bible",
+  pages: 1000}
+for (let key in obj) {
+  console.log(key);
+}
---
+++ TASK 2.js
@@ -0,0 +1,9 @@
+const a = 'hello';
+const b = {Object};
+console.log('a'  in Object);
+
+'a' in Object; 
+
+'b' in Object; 
+
+'c' in Object; 
